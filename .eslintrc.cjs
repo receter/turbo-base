a5195149c57a160a415f34f303691e0f9abade9d
Andreas Riedmüller <receter@gmail.com>
2024-02-25 08:53:40 +0100

    update eslint configuration to enable type aware lint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,8 +3,10 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:react-hooks/recommended",
+    "react/recommended",
+    "plugin:react/jsx-runtime",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
@@ -14,5 +16,12 @@ module.exports = {
       "warn",
       { allowConstantExport: true },
     ],
+  },
+  // other rules...
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+    tsconfigRootDir: __dirname,
   },
 };
